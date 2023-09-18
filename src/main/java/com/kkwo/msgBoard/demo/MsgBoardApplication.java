4c521f0b67e5ed8b1ed61cc21a4d3037cfd2e8df
kkwonju <kkwo4140@gmail.com>
2023-09-18 19:34:04 +0900

    2023 게시판 연습 시작

---
+++ src/main/java/com/kkwo/msgBoard/demo/MsgBoardApplication.java
@@ -0,0 +1,13 @@
+package com.kkwo.msgBoard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkwo/msgBoard/demo/MsgBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkwo.msgBoard.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
