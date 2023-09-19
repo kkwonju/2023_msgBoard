21e972d5f7af53497b5bba117fc27e9264e44bdd
kkwonju <kkwo4140@gmail.com>
2023-09-19 20:52:34 +0900

    준비

---
+++ src/main/java/com/kkwo/msgBoard/demo/controller/UsrMemberController.java
@@ -0,0 +1,13 @@
+package com.kkwo.msgBoard.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsrMemberController {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		return "usr/article/main";
+	}
+}
